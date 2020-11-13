ce9052f324ddfb1faccff5a7421424ee5b966e0d
yahatsuki <kabas3d@gmail.com>
2020-11-13 15:37:47 +0100

    Update ubuntu_docker.sh

--- ubuntu_docker.sh
+++ ubuntu_docker.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get remove -y docker docker-engine docker.io containerd runc
 sudo apt-get update
 
-sudo apt-get install -y \
+sudo apt-get install \
 apt-transport-https \
 ca-certificates \
 curl \
@@ -12,6 +12,7 @@ gnupg-agent \
 software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 sudo add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) \
