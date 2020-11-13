e101487bcde3ac3135cc4ba5c9841d7d8f8058ce
yahatsuki <kabas3d@gmail.com>
2020-11-13 15:18:37 +0100

    Update ubuntu_fresh.sh

--- ubuntu_fresh.sh
+++ ubuntu_fresh.sh
@@ -18,8 +18,9 @@ sudo mkdir /var/lib/clamav
 sudo systemctl start clamav-freshclam
 
 sudo apt-get install -y unattended-upgrades
-echo -e "APT::Periodic::Update-Package-Lists \"1\";\nAPT::Periodic::Unattended-Upgrade \"1\";\n" > /etc/apt/apt.conf.d/20auto-upgrades
-sudo /etc/init.d/unattended-upgrades restart
+sudo /etc/init.d/unattended-upgrades start
 
 sudo apt-get install xe-guest-utilities
+
+reboot
 
