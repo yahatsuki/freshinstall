c6d6b7d9afa3534eded44f6fd5ad565183f9c2dd
yahatsuki <kabas3d@gmail.com>
2020-11-28 15:18:21 +0100

    Update ubuntu_default.sh

--- ubuntu_default.sh
+++ ubuntu_default.sh
@@ -4,11 +4,12 @@ echo -n "New Hostname?"
 read hostname
 sudo hostnamectl set-hostname $hostname
 
-#echo -n "New Username?"
-#read username
+
 #sudo groupadd $username
 #sudo usermod -d /home/$username -m -g $username -l $username
 
+echo -n "New Username?"
+read username
 echo -n "New Password?"
 read password
 sudo passwd $username $password
