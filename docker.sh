45cf5eff6764af3ea28b65147239aaa48e11f9d3
yahatsuki <kabas3d@gmail.com>
2020-11-29 16:45:18 +0100

    Update and rename ubuntu_docker.sh to docker.sh

--- docker.sh
+++ docker.sh
@@ -24,7 +24,7 @@ sudo apt install -y docker-compose
 
 echo -n "Username?"
 read username
-sudo usermod -aG docker $username
+sudo usermod -a -G docker $username
 
 docker volume create portainer_data	
 docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
